ac68a1a553f7cf3a49204f4c8f1e55ad24534965
test <test@example.com>
2024-11-13 17:56:17 +0900

    update update.sh and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 dpkg -i assets/apktool_2.6.1_aarch64.deb
+apt install python git ncurses-utils apksigner openjdk-17
 apt install aapt aapt2 -y
 cp assets/aapt $PREFIX/bin
 cp assets/aapt2 $PREFIX/bin
